7c14130eb0c68fada2f7cc2d17cc11b27ba2f5f0
Luma Montes <lumagoesmontes@gmail.com>
2021-11-20 23:10:27 -0300

    finalizado trabalho

--- src/pages/categories/index.js
+++ src/pages/categories/index.js
@@ -1,14 +1,63 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+    Button, View, Text, SafeAreaView,
+    TextInput, ActivityIndicator, TouchableOpacity
+} from 'react-native';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+
+const validationSchema = yup.object().shape({
+    name: yup.string().required().label('Name'),
+});
 
 export default function Categories() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-      </View>
+        <View style={{ flex: 0.4, justifyContent: 'space-around', marginLeft: 10 }}>
+            <Text >Cadastro de categorias</Text>
+            <Formik
+                initialValues={{ name: '' }}
+                onSubmit={(values, actions) => {
+                    setTimeout(() => {
+                        actions.setSubmitting(false);
+                    }, 1000);
+                }}
+                validationSchema={validationSchema}
+            >
+                {(formikProps) => (
+                    <React.Fragment>
+                        <TextInput
+                            style={{
+                                borderWidth: 1,
+                                borderColor: 'black',
+                                padding: 10,
+                                width: "90%",
+                                borderRadius: 10,
+
+                            }}
+                            onChangeText={formikProps.handleChange('name')}
+                            placeholder='Nome'
+                        />
+                        <Text style={{ color: 'red' }}>{formikProps.errors.name}</Text>
+                        {formikProps.isSubmitting ? (
+                            <ActivityIndicator />
+                        ) : (
+                                <TouchableOpacity title="Submit" onPress={formikProps.handleSubmit} style={{
+                                    borderWidth: 1,
+                                    borderColor: 'black',
+                                    backgroundColor: '#7ea4e5',
+                                    padding: 20,
+                                    marginBottom: 3,
+                                    width: "90%",
+                                    borderRadius: 6,
+                                }}> 
+                                    <Text style={{color: 'white', textAlign: 'center', fontWeight: 'bold'}}>Cadastrar</Text>
+                                </TouchableOpacity>
+                            )}
+                    </React.Fragment>
+                )}
+            </Formik>
+        </View>
     );
-  }
+}
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,20 +1,39 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { StyleSheet, Button, View, Text, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LottieView from 'lottie-react-native';
 
 export default function Home({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Cadastrar produto"
-        onPress={() => navigation.navigate('Produto')}
-      />
-      <Button
-        title="Cadastrar categoria"
-        onPress={() => navigation.navigate('Categoria')}
-      />
-    </View>
-  );
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <LottieView
+                style={{
+                    width: 400,
+                    height: 400,
+                    backgroundColor: '#eee',
+                }}
+                source={require('../../../assets/69336-add-product.json')}
+                autoPlay={true} loop={true}
+            />
+            <TouchableOpacity style={styles.button} title="Cadastrar categoria" onPress={() => navigation.navigate('Produto')} >
+                <Text style={{ color: 'white', textAlign: 'center', fontWeight: 'bold' }}>Cadastrar produto</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} title="Cadastrar categoria" onPress={() => navigation.navigate('Categoria')} >
+                <Text style={{ color: 'white', textAlign: 'center', fontWeight: 'bold' }}>Cadastrar categoria</Text>
+            </TouchableOpacity>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    button: {
+        borderWidth: 1,
+        borderColor: 'black',
+        backgroundColor: '#7ea4e5',
+        padding: 20,
+        marginBottom: 3,
+        width: "90%",
+        borderRadius: 6,
+    },
+});
